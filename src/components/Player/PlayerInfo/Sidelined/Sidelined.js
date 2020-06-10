4a5a9489c4e182235d837ea0e2dade137856da28
igor29 <igmatviytsev@gmail.com>
2020-06-10 13:09:50 +0300

    Add sidelines to player component

--- src/components/Player/PlayerInfo/Sidelined/Sidelined.js
+++ src/components/Player/PlayerInfo/Sidelined/Sidelined.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-const Sidelined = () => {
-  return (<div>Sidelined</div>)
+import { Spin, Empty } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+
+import { fetchPlayerSidelinesActionCreator } from '../../../../store/player/actions';
+import classes from './Sidelined.module.css';
+
+const Sidelined = (props) => {
+  const { stats, sidelined, fetchPlayerSidelines } = props;
+
+  useEffect(() => {
+    if (stats.length === 0) return;
+
+    const playerId = stats[0].player_id;
+    fetchPlayerSidelines(playerId)
+  }, [stats, fetchPlayerSidelines])
+
+  if (sidelined.length === 0 && stats.length === 0) return <Spin />;
+  if (sidelined.length === 0) return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
+
+  return (
+    <div className={classes.sidelined}>
+      {
+        sidelined.map((sideline, index) => (
+          <div key={index} className={classes.sideline}>
+            <div className={classes.icon}><CloseOutlined /></div>
+            <div>{sideline.start}</div>
+            <div>{sideline.end}</div>
+            <div>{sideline.type}</div>
+          </div>
+        ))
+      }
+    </div>
+  )
 }
 
-export default Sidelined;
+const mapStateToProps = (props) => {
+  return {
+    stats: props.plr.stats,
+    sidelined: props.plr.sidelined,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchPlayerSidelines: () => { dispatch(fetchPlayerSidelinesActionCreator()) },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidelined);
