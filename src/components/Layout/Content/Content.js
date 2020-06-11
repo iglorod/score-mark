ba31d0590cf56ce69325a73f6f8c3f5603a6e55f
igor29 <igmatviytsev@gmail.com>
2020-06-11 16:38:40 +0300

    Add page transition animation

--- src/components/Layout/Content/Content.js
+++ src/components/Layout/Content/Content.js
@@ -11,13 +11,14 @@ import League from '../../League/League';
 import Club from '../../Club/Club';
 import Player from '../../Player/Player';
 import News from '../../News/News';
+import AnimatedSwitch from '../../UI/AnimatedSwitch/AnimatedSwitch';
 
 const ContentComponent = () => {
   const { Content } = Layout;
 
   return (
     <Content className='content-layout-background'>
-      <Switch>
+      <AnimatedSwitch classProp={'pageComponentWrapper'}>
         <Route path='/sign-up' component={SignUp} exact />
         <Route path='/sign-in' component={SignIn} exact />
         <Route path='/fixture' component={Fixture} exact />
@@ -32,7 +33,7 @@ const ContentComponent = () => {
         <Route path='/player' component={Admin} exact />
         <Route path='/' component={Main} /> */}
         <Route path='/' component={News} />
-      </Switch>
+      </AnimatedSwitch>
     </Content>
   )
 }
---
+++ src/components/UI/AnimatedSwitch/AnimatedSwitch.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { spring, AnimatedSwitch } from 'react-router-transition';
+
+import classes from './AnimatedSwitch.module.css';
+
+const animatedSwitch = (props) => {
+    const mapStyles = (styles) => {
+        return {
+            opacity: styles.opacity,
+            transform: `scale(${styles.scale})`,
+        };
+    }
+
+    const bounce = (val) => {
+        return spring(val, {
+            stiffness: 100,
+            damping: 20,
+        });
+    }
+
+    const bounceTransition = {
+        atEnter: {
+            opacity: 0,
+            scale: 0.8,
+        },
+        atLeave: {
+            opacity: bounce(0),
+            scale: bounce(0.8),
+        },
+        atActive: {
+            opacity: bounce(1),
+            scale: bounce(1),
+        },
+    };
+
+    let switchClass = classes.pageWrapper;
+    if (props.classProp) switchClass = classes[props.classProp]; 
+
+    return (
+        <AnimatedSwitch
+            atEnter={bounceTransition.atEnter}
+            atLeave={bounceTransition.atLeave}
+            atActive={bounceTransition.atActive}
+            mapStyles={mapStyles}
+            className={switchClass}
+        >
+            {props.children}
+        </AnimatedSwitch>
+    )
+}
+
+export default animatedSwitch;
